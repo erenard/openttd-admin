209364643b26e7ccb9e9f13def66bff17b17be8e
erenard <timmy469@gmail.com>
2016-07-03 11:06:03 +0200

    1.6.1

--- src/com/openttd/network/core/Configuration.java
+++ src/com/openttd/network/core/Configuration.java
@@ -7,8 +7,8 @@ public class Configuration {
 	public String password = "";
 	public String name = "openttd";
 	/* See rev.cpp.in and .ottdrev */
-	private final int version[] = {1, 5, 3};
+	private final int version[] = {1, 6, 1};
 	public final String openttdVersion = version[0] + "." + version[1] + "." + version[2];
-	private final long revision = 27462;
+	private final long revision = 27609;
 	public final long openttdNewgrfVersion = version[0] << 28 | version[1] << 24 | version[2] << 20 | 1 << 19 | (revision & ((1 << 19) - 1));
 }
