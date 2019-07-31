ef4741969fc5454b408c59512d6b14ce870b8d1e
Eric Renard <timmy469@gmail.com>
2019-07-31 18:29:58 +0200

    Bump to OpenTTD 1.9.2

--- src/com/openttd/network/core/Configuration.java
+++ src/com/openttd/network/core/Configuration.java
@@ -7,8 +7,8 @@ public class Configuration {
 	public String password = "";
 	public String name = "openttd";
 	/* See rev.cpp.in and .ottdrev */
-	private final int version[] = {1, 8, 0};
+	private final int version[] = {1, 9, 2};
 	public final String openttdVersion = version[0] + "." + version[1] + "." + version[2];
-	private final long revision = 28002;
-	public final long openttdNewgrfVersion = version[0] << 28 | version[1] << 24 | version[2] << 20 | 1 << 19 | (revision & ((1 << 19) - 1));
+	private final long revision = 28004;
+	public final long openttdNewgrfVersion = version[0] << 28 | version[1] << 24 | version[2] << 20 | 1 << 19 | revision;
 }
