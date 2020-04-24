7d873db46a0febc9dc89c5534923820cc6d37e44
Éric Renard <eric.renard@toucantoco.com>
2020-04-24 23:03:30 +0200

    OpenTTD 1.10.1

--- src/com/openttd/network/core/Configuration.java
+++ src/com/openttd/network/core/Configuration.java
@@ -7,8 +7,8 @@ public class Configuration {
 	public String password = "";
 	public String name = "openttd";
 	/* See rev.cpp.in and .ottdrev */
-	private final int version[] = {1, 10, 0};
+	private final int version[] = {1, 10, 1};
 	public final String openttdVersion = version[0] + "." + version[1] + "." + version[2];
 	private final long revision = 28004;
-	public final long openttdNewgrfVersion = version[0] << 28 | version[1] << 24 | version[2] << 20 | 1 << 19 | revision;
+	public final long openttdNewgrfVersion = version[0] << 28 | version[1] << 24 | 0 << 20 | 1 << 19 | revision;
 }
